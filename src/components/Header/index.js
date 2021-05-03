6298c71db441e39fe1983f1e4ebd67f879f00bcd
Heather Smith <halexsmith86@gmail.com>
2021-05-03 17:06:30 -0400

    Added html for Home and the navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div></div>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark justify-content-center">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link
+            to="/home"
+            className={
+              window.location.pathname === "/home"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/home"
+            className={
+              window.location.pathname === "/portfolio"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/home"
+            className={
+              window.location.pathname === "/contact"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,101 @@
 import React from "react";
+
+function Home() {
+  return (
+    <div className="content">
+      <div className="box">
+        <h1 className="display-1">Heather Smith</h1>
+        <em>full stack developer</em>
+      </div>
+
+      <div className="container mb-4" id="aboutme">
+        <div className="row justify-content-center">
+          <div className="col-sm-12">
+            <img
+              src="../../public/img/bio-picture.png"
+              className="img-thumbnail float-start"
+              id="profile-img"
+              alt="Heather Smith"
+            />
+
+            <p>
+              <em>
+                <b>Heather Smith</b>
+              </em>{" "}
+              is a developer currently enrolled in Georgia Tech's Full Stack
+              Flex Bootcamp. Her skills include:
+            </p>
+
+            <div id="skill-list">
+              <ul className="list-group d-inline-flex">
+                <li className="list-group-item list-group-item-action text-center">
+                  JavaScript
+                </li>
+                <li className="list-group-item list-group-item-action text-center">
+                  jQuery
+                </li>
+                <li className="list-group-item list-group-item-action text-center">
+                  Node
+                </li>
+                <li className="list-group-item list-group-item-action text-center">
+                  Postman
+                </li>
+                <li className="list-group-item list-group-item-action text-center">
+                  Express
+                </li>
+                <li className="list-group-item list-group-item-action text-center">
+                  Handlebars
+                </li>
+              </ul>
+
+              <ul className="list-group d-inline-flex">
+                <li className="list-group-item list-group-item-action text-center li2">
+                  HTML
+                </li>
+                <li className="list-group-item list-group-item-action text-center li2">
+                  CSS
+                </li>
+                <li className="list-group-item list-group-item-action text-center li2">
+                  Bootstrap
+                </li>
+                <li className="list-group-item list-group-item-action text-center li2">
+                  MongoDB
+                </li>
+                <li className="list-group-item list-group-item-action text-center  li2">
+                  mySQL
+                </li>
+                <li className="list-group-item list-group-item-action text-center li2">
+                  Sequelize
+                </li>
+              </ul>
+            </div>
+            <br />
+            <p>
+              Before the pandemic, Heather was working as an office
+              administrator for a small label printing company. As the pandemic
+              spread, she left her job and started self-guided studies with
+              Codecademy at home. After learning through the summer and fall,
+              she had built a foundation and it was time to move forward. The GA
+              Tech bootcamp has been an enormous challenge, but she is on track
+              to graduate at the end of May, 2021.
+              <br />
+              <br />
+              <img
+                src="../../public/img/kitties.jpg"
+                className="img-thumbnail float-end"
+                id="kitties"
+                alt="Bun and Mia, two cats"
+              />
+              When she isn't coding, Heather likes gardening, cooking, thrift
+              shopping, and video games. She also loves to fossil hunt, and is a
+              member of the Georgia Mineral Society. Currently, she lives in
+              Sandy Springs, Georgia with her husband and two cats.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
