8b25f4d2666d8b73308b25e5eeb3cda3d834a84f
Heather Smith <halexsmith86@gmail.com>
2021-05-07 17:15:16 -0400

    Finished projects object

--- src/components/Project/projects.js
+++ src/components/Project/projects.js
@@ -1,4 +1,3 @@
-
 //     "img": ['../../img/electric-sheep.png', '../../img/code-quiz-screenshot.png', '../../img/note-taker.png', '../../img/planner-screenshot.png', '../../img/weather-screenshot.png', '../../img/tracker.png'],
 
 //     "cardtitle": ['Electric Sheep', 'OpenWeather Dashboard', 'Work Day Scheduler', 'Coding Quiz Challenge', 'Note Taker', 'Employee Tracker'],
@@ -11,22 +10,45 @@
 // }];
 
 export const projects = [
-    {
-        img: '../../img/electric-sheep.png',
-        cardtitle: 'Electric Sheep',
-        cardtext: 'A dream journal, made in collaboration with two classmates.',
-        deployedlink: 'https://banana-crumble-13553.herokuapp.com/',
-        githublink: "https://github.com/acbewley/young-vipers-electric-sheep"},
-    {
-       img: '../../img/code-quiz-screenshot.png',
-       cardtitle: 'OpenWeather Dashboard',
-       cardtext: 'A dashboard that displays the current weather and 5-day forecast.',
-       deployedlink: "https://sorengrey.github.io/OpenWeather-Dashboard/",
-       githublink: "https://sorengrey.github.io/OpenWeather-Dashboard/"},
-//     {},
-//     {},
-//     {},
-//     {}
- ]
+  {
+    img: "../../img/electric-sheep.png",
+    cardtitle: "Electric Sheep",
+    cardtext: "A dream journal, made in collaboration with two classmates.",
+    deployedlink: "https://banana-crumble-13553.herokuapp.com/",
+    githublink: "https://github.com/acbewley/young-vipers-electric-sheep",
+  },
+  {
+    img: "../../img/weather-screenshot.png",
+    cardtitle: "OpenWeather Dashboard",
+    cardtext: "A dashboard that displays the current weather and 5-day forecast.",
+    deployedlink: "https://sorengrey.github.io/OpenWeather-Dashboard/",
+    githublink: "https://github.com/sorengrey/OpenWeather-Dashboard",
+  },
+  {
+    img: "../../img/note-taker.png",
+    cardtitle: "Note Taker",
+    cardtext: "An application that allows the user to take and save notes to its server.",
+    deployedlink: "https://arcane-sands-37553.herokuapp.com/",
+    githublink: "https://github.com/sorengrey/Note-Taker",
+  },
+{    img: "../../img/code-quiz-screenshot.png",
+      cardtitle: "Coding Quiz Challenge",
+      cardtext: "A quiz game with a timer and a high score board.",
+      deployedlink: "https://sorengrey.github.io/Coding-Quiz",
+      githublink: "https://github.com/sorengrey/Coding-Quiz",
+    },
+{    img: "../../img/planner-screenshot.png",
+      cardtitle: "Work Day Scheduler",
+      cardtext: "An application that lets the user schedule their workday.",
+      deployedlink: "https://sorengrey.github.io/Day-Planner/",
+      githublink: "https://github.com/sorengrey/Day-Planner",
+    },
+{    img: "../../img/tracker.png",
+      cardtitle: "Employee Tracker",
+      cardtext: "A CLI program that keeps track of a company's employees.",
+      deployedlink: "https://github.com/sorengrey/Employee-Tracker-CMS",
+      githublink: "https://github.com/sorengrey/Employee-Tracker-CMS",
+    }
+];
 
- export default projects;
+export default projects;
