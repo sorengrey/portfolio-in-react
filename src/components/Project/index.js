5b9d787b82584db55d9719f369ab3b20f7b8fb0f
Heather Smith <halexsmith86@gmail.com>
2021-05-07 13:27:12 -0400

    Created style.css and json for Projects component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import proj1 from ''
+import sheep from "../../img/electric-sheep.png"
 import proj2 from ''
 import proj3 from ''
 import proj4 from ''
@@ -8,7 +8,33 @@ import proj6 from ''
 
 function Project() {
     return (
-        <div></div>
+        <div className="card float-start">
+              <img
+                src={sheep}
+                className="card-img-top img-thumbnail"
+                alt="..."
+              />
+              <div className="card-body">
+                <div className="card-title text-center">
+                  <h5>Electric Sheep</h5>
+                </div>
+                <p className="card-text">
+                  A dream journal, made in collaboration with two classmates.
+                </p>
+                <a
+                  href="https://banana-crumble-13553.herokuapp.com/"
+                  className="card-link"
+                >
+                  Live Application
+                </a>
+                <a
+                  href="https://github.com/acbewley/young-vipers-electric-sheep"
+                  className="card-link"
+                >
+                  Github Repo
+                </a>
+              </div>
+            </div>
     )
 }
 
